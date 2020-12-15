f631bf02a271c6ff46a204aca0bbbd00c47b2177
Manuel <mramirez.prz.97@outlook.com>
2020-12-14 23:13:53 -0600

    Added: - Component, activity and Layout for Single Movie View - Basic UI for SingleMovie view - Data packages - Started NetworkState for REST API consume
    Fixed:
    -Nothing to show

---
+++ app/src/main/java/com/marp/testapptmdb/single_movie/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.marp.testapptmdb.single_movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marp.testapptmdb.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
